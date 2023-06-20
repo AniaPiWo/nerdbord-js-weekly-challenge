11c5397d0e5b56296b5f33cd6778650d848f48d8
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-06-20 13:39:31 +0200

    starter 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import { Stars } from '../challenges/week-1/Stars' /* <Stars rows={7} columns={20}/> */
-import { BreakingNews } from '../challenges/week-2/BreakingNews'
-import { ConfirmationModal } from '../challenges/week-3/ConfirmationModal'
-import { InteractiveProductCard } from '../challenges/week-4/InteractiveProductCard'
+import "./App.css";
+import { Stars } from "../challenges/week-1/Stars"; /* <Stars rows={7} columns={20}/> */
+import { BreakingNews } from "../challenges/week-2/BreakingNews";
+import { ConfirmationModal } from "../challenges/week-3/ConfirmationModal";
+import { InteractiveProductCard } from "../challenges/week-4/InteractiveProductCard";
+import { WebsiteProject } from "../challenges/week-5/WebsiteProject";
+import { DarkMode } from "../challenges/week-7/DarkMOde";
 
 function App() {
-
   return (
-
-      <div>
-        <InteractiveProductCard />
-      </div>
-  )
+    <div>
+      <DarkMode />
+    </div>
+  );
 }
 
-export default App
+export default App;

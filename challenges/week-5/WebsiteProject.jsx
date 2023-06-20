b3244993cc79484fc33d502756acb3d250578d25
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-06-20 13:39:38 +0200

    starter 7

---
+++ challenges/week-5/WebsiteProject.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import css from "./WebsiteProject.module.css";
+
+export const WebsiteProject = () => {
+  return (
+    <div>
+      <div>
+        <h1></h1>
+        <p></p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
---
+++ challenges/week-7/DarkMode.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DarkMode = () => {
+  return <div>darkmode</div>;
+};

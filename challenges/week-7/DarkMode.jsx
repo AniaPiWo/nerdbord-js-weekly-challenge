ea7c81ac619e81c7969213ebf78dffaefc6d87df
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-06-20 14:36:22 +0200

    darkmode

--- challenges/week-7/DarkMode.jsx
+++ challenges/week-7/DarkMode.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./DarkMode.module.css";
 import logo from "./img/logo.png";
 
 export const DarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const containerClassName = `${css.container} ${
+    isDarkMode ? css.dark : css.light
+  }`;
+
   return (
-    <div>
+    <div className={containerClassName}>
       <div className={css.header}>
         <div className={css.logo}>
           <img src={logo} alt="" />
           <div className={css.logoDot}></div>
         </div>
-        <div className={css.toggle}></div>
+        <div className={css.toggle} onClick={toggleDarkMode}></div>
         <ul className={css.nav}>
           <li className={css.navItem}>Projects</li>
           <li className={css.navItem}>Services</li>
@@ -19,7 +29,10 @@ export const DarkMode = () => {
         </ul>
       </div>
       <div className={css.main}>
-        <span lassName={css.title}>Here goes the sun here comes a warmth.</span>
+        <span className={css.title}>
+          Here goes the sun
+          <br /> here comes a warmth.
+        </span>
       </div>
     </div>
   );

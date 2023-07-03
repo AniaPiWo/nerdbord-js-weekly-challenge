54442dae157cb1469391ffaeaaebd17c4e80408e
AniaPiWo <snowflake@wp.pl>
2023-07-03 19:34:25 +0200

    transform to next

---
+++ app/week-6/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Week6 = () => {
+  return (
+    <div>
+        Coming soon!
+    </div>
+  )
+}
+
+export default Week6
---
+++ app/week-7/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Week7 = () => {
+  return (
+    <div>
+        Coming soon!
+    </div>
+  )
+}
+
+export default Week7
